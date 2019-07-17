40e89c3486ddf6c8c59ae2d5e864af044bf32a6e
takanaka-1024 <riyokokanaketa@gmail.com>
2019-07-17 20:06:00 +0900

    gem編集

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,7 +15,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
 
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [200, 200]
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
